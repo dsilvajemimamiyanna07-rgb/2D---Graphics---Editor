1eb405f7a00a3bb8b084bb3a15f95af8d504305a
dsilvajemimamiyanna07-rgb <dsilvajemimamiyanna07@gmail.com>
2026-06-09 10:55:36 +0000

    Added line canvas

--- graphics_editor.c
+++ graphics_editor.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define ROWS 25
 #define COLS 60
 
 char rectCanvas[ROWS][COLS];
+char lineCanvas[ROWS][COLS];
 
-void initialize()
+/* Initialize Canvas */
+void initialize(char canvas[ROWS][COLS])
 {
     int i, j;
 
@@ -13,12 +16,13 @@ void initialize()
     {
         for(j = 0; j < COLS; j++)
         {
-            rectCanvas[i][j] = '_';
+            canvas[i][j] = '_';
         }
     }
 }
 
-void display()
+/* Display Canvas */
+void display(char canvas[ROWS][COLS])
 {
     int i, j;
 
@@ -28,14 +32,16 @@ void display()
     {
         for(j = 0; j < COLS; j++)
         {
-            printf("%c", rectCanvas[i][j]);
+            printf("%c", canvas[i][j]);
         }
-
         printf("\n");
     }
 }
 
-void drawRectangle(int x, int y, int width, int height)
+/* Feature 1: Draw Rectangle */
+void drawRectangle(char canvas[ROWS][COLS],
+                   int x, int y,
+                   int width, int height)
 {
     int i, j;
 
@@ -43,23 +49,116 @@ void drawRectangle(int x, int y, int width, int height)
     {
         for(j = x; j < x + width && j < COLS; j++)
         {
-            rectCanvas[i][j] = '*';
+            canvas[i][j] = '*';
         }
+    }
+}
+
+/* Feature 2: Draw Line */
+void drawLine(char canvas[ROWS][COLS],
+              int x1, int y1,
+              int x2, int y2)
+{
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float xInc = dx / (float)steps;
+    float yInc = dy / (float)steps;
+
+    float x = x1;
+    float y = y1;
+
+    int i;
+
+    for(i = 0; i <= steps; i++)
+    {
+        if((int)x >= 0 && (int)x < COLS &&
+           (int)y >= 0 && (int)y < ROWS)
+        {
+            canvas[(int)y][(int)x] = '*';
+        }
+
+        x += xInc;
+        y += yInc;
     }
 }
 
 int main()
 {
-    int x, y, w, h;
+    int choice;
 
-    initialize();
+    initialize(rectCanvas);
+    initialize(lineCanvas);
 
-    printf("Enter x y width height: ");
-    scanf("%d%d%d%d", &x, &y, &w, &h);
+    do
+    {
+        printf("\n===== GRAPHICS EDITOR =====\n");
+        printf("1. Draw Rectangle\n");
+        printf("2. Draw Line\n");
+        printf("3. Display Rectangle Canvas\n");
+        printf("4. Display Line Canvas\n");
+        printf("5. Exit\n");
 
-    drawRectangle(x, y, w, h);
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
 
-    display();
+        switch(choice)
+        {
+            case 1:
+            {
+                int x, y, w, h;
+
+                initialize(rectCanvas);
+
+                printf("Enter x y width height: ");
+                scanf("%d%d%d%d", &x, &y, &w, &h);
+
+                drawRectangle(rectCanvas, x, y, w, h);
+
+                printf("Rectangle Drawn Successfully\n");
+                break;
+            }
+
+            case 2:
+            {
+                int x1, y1, x2, y2;
+
+                initialize(lineCanvas);
+
+                printf("Enter x1 y1 x2 y2: ");
+                scanf("%d%d%d%d",
+                      &x1, &y1,
+                      &x2, &y2);
+
+                drawLine(lineCanvas,
+                         x1, y1,
+                         x2, y2);
+
+                printf("Line Drawn Successfully\n");
+                break;
+            }
+
+            case 3:
+                printf("\nRECTANGLE CANVAS\n");
+                display(rectCanvas);
+                break;
+
+            case 4:
+                printf("\nLINE CANVAS\n");
+                display(lineCanvas);
+                break;
+
+            case 5:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid Choice\n");
+        }
+
+    } while(choice != 5);
 
     return 0;
 }
